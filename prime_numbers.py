15a7410b38207be7ff2d54d570acd7d7f3458ccb
cfeen1 <45612494+cfeen1@users.noreply.github.com>
2019-01-14 10:41:23 -0500

    Update prime_numbers.py

--- prime_numbers.py
+++ prime_numbers.py
@@ -1,18 +1,17 @@
 def is_prime(input):
-	"""This functions goal is to check if an input number is prime"""
+	"""This functions goal is to check if an input number is prime."""
 	# I included a try-except block to account for a user inputing
-	# a string or a float
+	# a string or a float.
 	try:
-		# First, convert the input to an integer 
+		# First, the input is converted to an integer.  
 		integer_input = int(input)
-		# Check to see if it is positive
+		# Then, Python checks if it is a positive value.
 		if integer_input > 0:
-			# loop through a range of numbers from 2 to the input number
+			# A for loop is used here to go through each number from 2
+			# to the input number. 
 			for i in range(2, (integer_input)):
-				# for each number in the loop, divide input number
-				# by each number within the range and check if it 
-				# returns a remainder, if it does not, the number
-				# is not prime, so, exit the for loop
+				# The input_number is divided by each number in the for loops range.
+				# If no remainder is found, it is not prime and the loop is exited.  
 				if (integer_input % i) == 0:
 					return(str(integer_input) + " is not a prime number.")
 					break
@@ -25,7 +24,7 @@ def is_prime(input):
 									
 while True:
 	"""This while loop continuly asks the user for a number to check
-	if it is prime or until the user enters 'q' to quit"""
+	if it is prime or until the user enters 'q' to quit."""
 	prompt = "Enter a number and I will tell you if it is prime, "
 	prompt += "\nor enter 'q' to exit: "
 	ask = input(prompt)
